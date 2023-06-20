972b3a0469aa5bc3f34653fda4b76c337bd3499a
ShehalShah <102235406+ShehalShah@users.noreply.github.com>
2023-06-20 11:52:13 +0530

    topbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,10 @@
 import React from 'react';
 
 const Hero = () => {
-  const user = {
-    name: 'Pratik Jain',
-    position: 'Manager',
-    profileImage: 'image.jpg',
-  };
+  
 
   return (
-    <div className="text-green-500 w-5/6 ml-auto">
-      <div className="bg-gray-200 p-4 flex justify-between items-center">
-        <h1 className="text-xl font-bold">Stakeholder Management</h1>
-        <div className="flex items-center">
-          <img
-            src={user.profileImage}
-            alt="Profile"
-            className="w-8 h-8 rounded-full object-cover mr-2"
-          />
-          <div className='flex flex-col'>
-          <div className="font-bold">{user.name}</div>
-          <div>{user.position}</div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <></>
   );
 };
 
