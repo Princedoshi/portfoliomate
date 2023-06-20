864e651b6b449b5c291609a1f59beafeb82087a3
ShehalShah <102235406+ShehalShah@users.noreply.github.com>
2023-06-20 11:48:26 +0530

    topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import './App.css'
 import Sidebar from './components/sidebar'
 import Hero from './components/Hero'
 function App() {
   return (
-   <div>
+   <div >
      <Sidebar/>
      <Hero/>
    </div>
