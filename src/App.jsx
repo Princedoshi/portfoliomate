44aa01d54c1cabc7aac962ab82d761607db12507
PRINCE <pdoshi135@gmail.com>
2023-06-20 11:29:11 +0530

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './App.css'
 import Sidebar from './components/sidebar'
 import Hero from './components/Hero'
+import Navbar from './components/Navbar'
 function App() {
   return (
    <div>
      <Sidebar/>
+     <Navbar/>
      <Hero/>
    </div>
   )
